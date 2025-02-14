781a61babe975cc015fec2fd1750d5e7d218a011
TΛKU <tdmurwira@gmail.com>
2025-02-15 01:56:45 +0200

    Learning about the navigation and components in NEXT.js

---
+++ ukvt/app/components/hello.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+function Hello() {
+  console.log("hey big dawg");
+  return (
+    <div>
+      <h1>Hello again</h1>
+    </div>
+  );
+}
+
+export default Hello; // This is the default export of the module
---
+++ ukvt/app/dashboard/users/page.tsx
@@ -0,0 +1,22 @@
+const page = () => {
+  return (
+    <div>
+      <h1>Dashboard Users</h1>
+      <ul />
+      <li>
+        <a href="/dashboard/users/1">User 1</a>
+      </li>
+      <li>
+        <a href="/dashboard/users/2">User 2</a>
+      </li>
+      <li>
+        <a href="/dashboard/users/3">User 3</a>
+      </li>
+      <li>
+        <a href="/dashboard/users/4">User 4</a>
+      </li>
+    </div>
+  );
+};
+
+export default page;
